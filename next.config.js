f1582ca03004a3914cd17691b5a76bc44fac25bf
SyxNeet <110085910+SyxNeet@users.noreply.github.com>
2023-10-17 21:37:53 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const isProduction = process.env.NODE_ENV === "production";
 
-module.exports = nextConfig
+module.exports = {
+  future: {
+    webpack5: true,
+  },
+  ...(isProduction && {
+    // Cấu hình để tối ưu hóa và ghép nhất tệp CSS trong môi trường production
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.optimization.splitChunks.cacheGroups.styles = {
+          name: "styles",
+          test: /\.(css|scss)$/,
+          chunks: "all",
+          enforce: true,
+        };
+      }
+
+      return config;
+    },
+  }),
+};
